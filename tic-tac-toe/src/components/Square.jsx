ae7399aca0d64fe34865a8e35784b0e7e1c46fbb
LeonarDev <leonardo.majevski@gmail.com>
2021-03-23 20:03:44 -0300

    Adds event by clicking on the squere

--- tic-tac-toe/src/components/Square.jsx
+++ tic-tac-toe/src/components/Square.jsx
@@ -3,10 +3,9 @@ import React from 'react';
 export default class Square extends React.Component {
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button className="square" onClick={() => alert(`click on ${this.props.value}`) }>
+        { this.props.value }
       </button>
     );
   }
 }
-
