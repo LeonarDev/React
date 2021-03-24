dec72298892f87b6399879ca7f3478212715efe3
LeonarDev <leonardo.majevski@gmail.com>
2021-03-24 18:39:33 -0300

    Ignore the click if there is a winner or the Square is already filled

--- tic-tac-toe/src/components/Board.jsx
+++ tic-tac-toe/src/components/Board.jsx
@@ -14,6 +14,10 @@ export default class Board extends React.Component {
   handleClick(i) {
     const squares = this.state.squares.slice();
 
+    if (this.calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     
     this.setState({
